e96bb65db2c4f2044b3d730ca13ff925a228f8ef
bbcy777 <bbcy777@gmail.com>
2024-05-28 23:06:54 -0700

    promise

--- src/index.mjs
+++ src/index.mjs
@@ -1,33 +1,59 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.mjs";
 
-async function getUserData(id) {
+// async function getUserData(id) {
+//   const dbs = {
+//     db1: db1,
+//     db2: db2,
+//     db3: db3
+//   };
+//   try {
+//     const dbValue = await central(id);
+//     const dbWeb = await dbs[dbValue](id);
+//     const userData = await vault(id);
+//     return {
+//       id: id,
+//       name: userData.name,
+//       username: dbWeb.username,
+//       email: userData.email,
+//       address: userData.address,
+//       phone: userData.phone,
+//       website: dbWeb.website,
+//       company: dbWeb.company,
+//     }
+//   } catch(error) {
+//       return Promise.reject(`Error: ${error}`)
+//   }  
+// }
+
+function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
-  try {
-    const dbValue = await central(id);
-    const dbWeb = await dbs[dbValue](id);
-    const userData = await vault(id);
-    return {
-      id: id,
-      name: userData.name,
-      username: dbWeb.username,
-      email: userData.email,
-      address: userData.address,
-      phone: userData.phone,
-      website: dbWeb.website,
-      company: dbWeb.company,
-    }
-  } catch(error) {
-      return Promise.reject(`Error: ${error}`)
-  }
-
-
-  
-  
+  return central(id)
+    .then((dbValue) => {
+      return dbs[dbValue](id);
+    })
+    .then((dbWeb) => {
+      return vault(id)
+        .then((userData) => {
+          return {
+            id: id,
+            name: userData.name,
+            username: dbWeb.username,
+            email: userData.email,
+            address: userData.address,
+            phone: userData.phone,
+            website: dbWeb.website,
+            company: dbWeb.company,
+          }
+        })
+    })
+    .catch(error => {
+      return Promise.reject(`Error: ${error}`);
+    });
 }
 
 getUserData(8)
